a37272b409d9fe92ff4786ca3322e3a313a294eb
Christian Staib <staib.christian@hotmail.de>
2024-12-02 10:55:56 +0100

    finished day_02

---
+++ day_02/src/bin/part1.rs
@@ -0,0 +1,73 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader, Read},
+};
+
+use itertools::Itertools;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file = File::open(&args[1]).unwrap();
+
+    let number_of_safe_reports = get_number_of_safe_reports(file);
+
+    println!("number of safe reports is {}", number_of_safe_reports);
+}
+
+fn get_number_of_safe_reports<R: Read>(reader: R) -> u32 {
+    // build list of reports
+    let reports = BufReader::new(reader)
+        .lines()
+        .map_while(Result::ok)
+        .map(|line| {
+            let report = line
+                .split_whitespace()
+                .flat_map(&str::parse::<u32>)
+                .collect::<Vec<_>>();
+
+            report
+        })
+        .collect::<Vec<_>>();
+
+    // count safe reports. Use abs_diff to avoid overflow
+    let number_of_safe_reports = reports
+        .iter()
+        .filter(|&report| {
+            let increasing_report = report
+                .iter()
+                .tuple_windows()
+                .all(|(&num0, &num1)| num0 < num1 && num0.abs_diff(num1) <= 3);
+
+            let decreasing_report = report
+                .iter()
+                .tuple_windows()
+                .all(|(&num0, &num1)| num0 > num1 && num0.abs_diff(num1) <= 3);
+
+            increasing_report || decreasing_report
+        })
+        .count() as u32;
+
+    number_of_safe_reports
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use crate::get_number_of_safe_reports;
+
+    #[test]
+    fn test_get_number_of_safe_reports() {
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+        let result = get_number_of_safe_reports(Cursor::new(input));
+        assert_eq!(result, 2);
+    }
+}
